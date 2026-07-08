e441f5857d05f5c7c728838c21db0d2fd4c3ef92
CarolPinerosTrujillo <caroll25m@gmail.com>
2026-07-08 16:19:55 -0500

    subida hasta punto 5

--- restaurante.js
+++ restaurante.js
@@ -43,11 +43,16 @@ function mostrarMenu(menu) {
   console.log("Categoria:",menu[i].categoria +" ,Nombre:",menu[i].nombre + " — " +",precio:", menu[i].precio);
 }  
 }
+console.log("RETO 1 - MOSTRAR MENU COMPLETO");
+console.log("================================== ");
+mostrarMenu(menu)
 
-//mostrarMenu(menu)
 
 
 //RETO 2 SOLO LOS PLATOS DISPONIBLES
+console.log("RETO 2 - MOSTRAR MENU PLATOS DISPONIBLES ");
+console.log("================================== ");
+
 function soloDisponibles(menu) {
 
     let disponibles = []  //array vacio para crear nuevo con disponibles
@@ -62,10 +67,9 @@ function soloDisponibles(menu) {
 }
 let resultado = soloDisponibles(menu);
 //COMPARACION LISTADO COMPLETO VS DISPONIBLES
-//console.log("MENU COMPLETO");
-//mostrarMenu(menu);
-//console.log("MENU DISPONIBLES");
-//mostrarMenu(resultado);
+
+console.log("MENU DISPONIBLES");
+mostrarMenu(resultado);
 
 
 //RETO 3 TOMAR PEDIDO DE UNA MESA 
@@ -79,24 +83,101 @@ function quitarUltimoPlato(pedido) {
     return pedido.pop();
 }
 
+function tomarPedido() {
+    let pedido = [];
 
 
+}
+
+console.log("RETO 3 - SIMULACION PEDIDO DE UNA MESA");
+console.log("================================== ");
 //CREANDO PEDIDO SIMULACION
 let pedido = [];
-
+//AGREGAR PLATOS
 agregarPlato(pedido, menu[0]);
 agregarPlato(pedido, menu[2]);
+agregarPlato(pedido, menu[4]);
 
+console.log("Pedido inicial" ,pedido);
+//QUITAR ULTIMO PLATO
 let eliminado = quitarUltimoPlato(pedido);
 
-console.log("Eliminado:", eliminado.nombre);
-console.log("Resumen de pedido ");
+console.log("Plato Eliminado:", eliminado.nombre);
 
-//ingreso de platos al pedido
-//pedido.push(menu[0]);
-//pedido.push(menu[3]);
-//pedido.push(menu[5]);
-//Quitar ultimo plato
-//let eliminado = pedido.pop();
+//IMPRIMIR PEDIDO
+console.log("Resumen de pedido completo");
+//console.log(pedido);
 
 mostrarMenu(pedido);
+
+
+//RETO 4 COBRAR CUENTA CON IVA
+
+console.log("RETO 4 - CALCULAR Y MOSTRAR LA CUENTA CON IVA ");
+console.log("================================== ");
+function precioConIva(curso) {
+ let subtotal = 0; //ACUMULADOR
+
+    for (let i = 0; i < pedido.length; i++) {
+        subtotal += pedido[i].precio;
+    }
+
+    let iva = subtotal * 0.19; //CALCULO IVA
+    let total = subtotal + iva;
+
+    return total;
+}
+
+let totalFinal = precioConIva(pedido);
+console.log("La cuenta con IVA es: " ,totalFinal);
+
+
+//RETO 5 MESERO TOMA PEDIDO 
+
+
+
+console.log("RETO 5 - MESERO TOMA PEDIDO POR TECLADO ");
+console.log("================================== ");
+
+let total = 0;
+let opcion = "";
+
+let pedidoNuevo=[];
+
+//mostrar menu solo disponibles
+do {
+  console.log("\n--- MENÚ ---");
+
+  for (let i = 0; i < menu.length; i++) {
+    console.log(i + ": " + menu[i].nombre + " - $" + menu[i].precio);  //USA MENU DISPONIBLE (resultado ANTES guardada dela funcion solo disponibles)
+  }
+
+  let numero = prompt("Elige un plato (número) o escribe TERMINAR:");
+
+  // TERMINAR PEDIDO
+  if (numero === "TERMINAR") {
+    break;
+  }
+
+  numero = Number(numero);
+
+  // VALIDAR  
+  if (numero >= 0 && numero < menu.length) {
+    pedidoNuevo.push(menu[numero]);
+    total = total + menu[numero].precio;
+    console.log("Agregado:", resultado[numero].nombre);
+  } else {
+    console.log(" Opción inválida, intenta de nuevo");
+  }
+
+} while (true);
+
+
+
+console.log("\n--- PEDIDO FINAL ---");
+
+for (let i = 0; i < pedidoNuevo.length; i++) {
+  console.log(pedidoNuevo[i].nombre);
+}
+
+console.log("TOTAL DEL PEDIDO: $" + precioConIva(total));
